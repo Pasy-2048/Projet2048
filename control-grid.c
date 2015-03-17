2b8cfb59cc3d0c17f20c0cc06c34bf16403e0149
Lejeune.sim <simon.lej17@gmail.com>
2015-03-17 10:25:38 +0100

    New ending T

--- control-grid.c
+++ control-grid.c
@@ -44,20 +44,18 @@ int main()
          break;
       }
     }
+    bool hE=0;
+    int i,j;
+    i=availMoves(g);
     while(!game_over(g)){
-        if(can_move(g,RIGHT)){
-            play(g,RIGHT);
-        }
-	else if(can_move(g,UP)){
-            play(g,UP);
-        }
-	else if(can_move(g,DOWN)){
-            play(g,DOWN);
-        }
-	else{
-            play(g,LEFT);
-        }
-    }
+    j=i%4;
+      if(j!=0){if(hE==0){if(j%2==1){play(g,LEFT);hE=1;}
+    else{play(g,RIGHT);}}
+        else{if(j>=2){play(g,RIGHT);hE=0;}
+    else{play(g,LEFT);}}}
+      i=availMoves(g);j=i/4;
+      if(j!=0){if(j>=2){play(g,UP);}
+        else if(i==4){play(g,DOWN);}}}
     show_grid(g);
     delete_grid(g);
     return 0;
