244d2c85a6c09c1c95fea81f186eb57f4dfe56b5
Alexis <alexis.richard@etu.u-bordeaux1.fr>
2015-04-12 19:24:26 +0200

    Sashiel

--- strategy2.c
+++ strategy2.c
@@ -6,13 +6,20 @@
 void free_memless_strat (strategy strat){
   free (strat);}
 
+dir Israfel(strategy s,grid g){
+	if(((bool*)s->mem)[1]==0)
+		return Shinji(s,g);
+	else
+		return Asuka(s,g);
+  
 dir Shinji(strategy s, grid g){
+	((bool*)s->mem)[1]==1;
 	int j=availMoves(g)%4;
 	if(j!=0){
-		if(*((bool*)s->mem)==0){
+		if(((bool*)s->mem)[0]==0){
 			if(j%2==1){
 				return LEFT;
-				*((bool*)s->mem)=1;
+				((bool*)s->mem)[0]=1;
 			}
 			else{
 				return RIGHT;
@@ -21,7 +28,7 @@ dir Shinji(strategy s, grid g){
 		else{
 			if(j>=2){
 				return RIGHT;
-				*((bool*)s->mem)=0;
+				((bool*)s->mem)[0]=0;
 			}
 			else{
 				return LEFT;
@@ -33,13 +40,17 @@ dir Shinji(strategy s, grid g){
 }
 
 dir Asuka(strategy s, grid g){
+	((bool*)s->mem)[1]==0;
 	int j=availMoves(g)/4;
-	if(j>=2){
-		return UP;
+	if(j!=0){
+		if(j>=2){
+			return UP;
+		}
+		else{
+			return DOWN;
+		}
 	}
-	else{
-		return DOWN;
-	}
+	return Shinj(s,g);
 }
 
 void Zeruel (strategy strat){
@@ -49,10 +60,9 @@ strategy A1_baltus_lejeune_richard_martin_fast(){
 	srand(time(NULL));
 	strategy s=malloc(sizeof(struct strategy_s));
 	s->name="PASY";
-	s->play_move=Shinji;
-	s->mem=malloc(sizeof(bool));
+	s->play_move=Israfel;
+	s->mem=calloc(2,sizeof(bool));
 	s->free_strategy=Zeruel;
-	*((bool*)(s->mem))=0;
 	return s;
 }
 int availMoves(grid g){
